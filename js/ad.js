ac09b82185a130880d63bf47c807a70c74f1be30
ayshmnmm <65579576+ayshmnmm@users.noreply.github.com>
2022-06-15 21:01:58 +0530

    some more ads

--- js/ad.js
+++ js/ad.js
@@ -1,6 +1,8 @@
 let ads = [
     { "title": "Chiguru", "img": "/img/chiguru.jpg", "url": "https://chigurucitech.com/" },
     { "title": "Sbk Homes", "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnPIl9E07DQ76UUSATkIwT476RZVg5OO8vLg&usqp=CAU", "url": "https://www.worldsbk.com/" },
+    { "title": "Intuit", "img": "/img/intuit.jpg", "url": "https://engg.cambridge.edu.in/intuit/" },
+    { "title": "Citech", "img": "/img/citech.jpg", "url": "https://engg.cambridge.edu.in/" },
 ];
 
 let r = Math.floor(Math.random() * ads.length);
@@ -8,7 +10,7 @@ let adtitle = ads[r]["title"];
 let adimg = ads[r]["img"];
 let adurl = ads[r]["url"];
 
-let rad = `<div class="modal-box flex flex-col w-80 lg:w-90 justify-center" >
+let rad = `<div class="modal-box flex flex-col w-80 md:w-[400px] justify-center" >
 <h3 class="font-bold text-lg" >AD: ${adtitle}</h3>
 <a href="${adurl}">
 <img src="${adimg}" class="w-full" alt="advert" i/>
